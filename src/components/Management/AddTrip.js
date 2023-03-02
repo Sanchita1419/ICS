ac863074c4b8c99ff712c9a334d0fad82881c648
Sanchita <sanchita.pandit@myelinfoundry.com>
2023-03-02 17:14:17 +0530

    integrate api for add trip and get trip

--- src/components/Management/AddTrip.js
+++ src/components/Management/AddTrip.js
@@ -4,68 +4,172 @@ import Input from "../UI/Input/Input";
 import Modal from "../UI/Modal/Modal";
 import Select from "react-select";
 import classes from "./AddTrip.module.css";
+import { axiosInstance } from "../../config";
 const AddTrip = (props) => {
-  const tripIdInputRef = useRef();
-  const vehicleIdInputRef = useRef();
-  const driverIdInputRef = useRef();
-
+  const customerIdInputRef = useRef();
+  const customerNameInputRef = useRef();
+  const vehicleRegNoInputRef = useRef();
+  // const driversInputRef1 = useRef("");
+  // const driversInputRef2 = useRef("");
+  // const driversInputRef3 = useRef("");
+  const driversInputRef1 = useRef(null);
+  const driversInputRef2 = useRef(null);
+  const driversInputRef3 = useRef(null);
   const startTimeInputRef = useRef();
   const endTimeInputRef = useRef();
-  const [selected, setSelected] = useState("");
+  const [formIsValid, setFormIsValid] = useState("");
   const [driverNo, setDriverNo] = useState(0);
 
   const navigate = useNavigate();
-  const addVehicleHandler = (e) => {
+
+  /** Adding new trip */
+  const addTripHandler = async (e) => {
     e.preventDefault();
-    const enteredtripId = tripIdInputRef.current.value;
-    const enteredvehicleId = vehicleIdInputRef.current.value;
-    const entereddriverId = driverIdInputRef.current.value;
+    const enteredcustomerId = customerIdInputRef.current.value;
+    const enteredcustomerName = customerNameInputRef.current.value;
+    const enteredvehicleRegNo = vehicleRegNoInputRef.current.value;
+    // const entereddriverName1 = driversInputRef1.current.value;
+    // const entereddriverName2 = driversInputRef2.current.value;
+    // const entereddriverName3 = driversInputRef3.current.value;
+    const entereddriverId1 = driversInputRef1.current.value;
+    const entereddriverId2 = driversInputRef2.current.value;
+    const entereddriverId3 = driversInputRef3.current.value;
 
-    const enteredstartTime = startTimeInputRef.current.value;
-    const enteredendTime = endTimeInputRef.current.value;
-
+    const enteredstartTime = startTimeInputRef.current.value.replace(/T/g, " ");
+    const enteredendTime = endTimeInputRef.current.value.replace(/T/g, " ");
+    // vehicle_No regex = ^[A-Z]{2}[ -][0-9]{1,2}(?: [A-Z])?(?: [A-Z]*)? [0-9]{4}$
     console.log("Adding");
-    // console.log(enteredname, enteredage, enteredgender, enteredlicenseNo);
-    navigate("/manage");
-    tripIdInputRef.current.value = "";
-    vehicleIdInputRef.current.value = "";
-    driverIdInputRef.current.value = "";
+    // console.log(entereddriverName1, entereddriverName2, entereddriverName3);
+    // console.log(entereddriverName1, entereddriverName2, entereddriverName3);
+    const url =
+      "https://2x8710joc3.execute-api.ap-south-1.amazonaws.com/addtrip";
+    const newTripData = {
+      PlannedStartTime: enteredstartTime + ":00",
+      PlannedEndTime: enteredendTime + ":00",
+      Veh_Reg_No: enteredvehicleRegNo,
+      CustomerID: parseInt(enteredcustomerId),
+      TripStatus: "Scheduled",
+      // Driver_Name_1: entereddriverName1,
+      // Driver_Name_2: entereddriverName2,
+      // Driver_Name_3: entereddriverName3,
+      DriverID_1: parseInt(entereddriverId1),
+      DriverID_2: parseInt(entereddriverId2),
+      DriverID_3: parseInt(entereddriverId3),
 
+      CustomerName: enteredcustomerName,
+    };
+    console.log(JSON.stringify(newTripData));
+    const response = await axiosInstance.post("/trip", newTripData);
+    console.log(response.data.message);
+    alert(response.data.message);
+
+    // try {
+    //   const response = await fetch(url, {
+    //     mode: "no-cors",
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(newTripData),
+    //   });
+    //   const responseJson = await response.json();
+    //   console.log("response", responseJson);
+    // } catch (err) {
+    //   console.log(err);
+    // }
+
+    // await fetch(url, {
+    //   mode: "no-cors",
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(newTripData),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((err) => console.log(err));
+
+    // console.log(
+    //   enteredstartTime + ":00",
+    //   enteredendTime + ":00",
+    //   enteredvehicleRegNo,
+    //   enteredcustomerId,
+    //   parseInt(entereddriverId1),
+    //   parseInt(entereddriverId2),
+    //   parseInt(entereddriverId3),
+    //   enteredcustomerName
+    // );
+
+    customerIdInputRef.current.value = "";
+    customerNameInputRef.current.value = "";
+    vehicleRegNoInputRef.current.value = "";
+    driversInputRef1.current.value = null;
+    driversInputRef2.current.value = null;
+    driversInputRef3.current.value = null;
     startTimeInputRef.current.value = "";
     endTimeInputRef.current.value = "";
   };
-  const options = [
-    { value: 1, label: "1" },
-    { value: 2, label: "2" },
-    { value: 3, label: "3" },
-  ];
-  const handleChange = (selectedOption) => {
-    setDriverNo(selectedOption.value);
-  };
-  let content = [];
-  for (let i = 1; i <= driverNo; i++) {
-    content.push(
-      <div className={classes.inputBox}>
-        <label>Driver Id {i}</label>
-        <input type="text" name="driverId" ref={driverIdInputRef} />
-      </div>
-    );
-  }
+  // const options = [
+  //   { value: 1, label: "1" },
+  //   { value: 2, label: "2" },
+  //   { value: 3, label: "3" },
+  // ];
+  // const handleChange = (selectedOption) => {
+  //   setDriverNo(selectedOption.value);
+  // };
+  // let content = [];
+  //for (let i = 1; i <= driverNo; i++) {
+  //  if(i===1){
+  //   const itemProps = {ref: driversInputRef1}
+  //   } else if(i===2){
+  //     const itemProps = {ref:driversInputRef2}
+  //   } else{
+  //     const itemProps={ref:driversInputRef3}
+  //   }
+  // content.push(
+  //   <div className={classes.inputBox}>
+  //     <label>Driver Name {i}</label>
+  //     <input type="text" name="driverId" ref={driversInputRef[i]} />
+  //   </div>
+  // );
+  //}
   console.log(driverNo);
   return (
     <Modal onClose={props.onClose}>
       <div className={classes.addTrip}>
         <h3>Add Trip</h3>
-        <form onSubmit={addVehicleHandler}>
+        <form onSubmit={addTripHandler}>
           <div className={classes.inputBox}>
-            <label>Trip Id</label>
-            <input type="text" name="tripId" ref={tripIdInputRef} />
+            <label>Customer Id</label>
+            <input
+              type="number"
+              name="customerId"
+              ref={customerIdInputRef}
+              required
+            />
           </div>
           <div className={classes.inputBox}>
-            <label>Vehicle Id</label>
-            <input type="text" name="vehicleId" ref={vehicleIdInputRef} />
+            <label>Customer Name</label>
+            <input
+              type="text"
+              name="customerName"
+              ref={customerNameInputRef}
+              required
+            />
           </div>
-          <div className={classes.selectBox}>
+          <div className={classes.inputBox}>
+            <label>Vehicle Reg No</label>
+            <input
+              type="text"
+              name="vehicleId"
+              ref={vehicleRegNoInputRef}
+              required
+            />
+          </div>
+          {/* <div className={classes.selectBox}>
             <label>No. of drivers</label>
 
             <Select
@@ -88,15 +192,42 @@ const AddTrip = (props) => {
               options={options}
               onChange={handleChange}
             />
-          </div>
           {content}
+          </div> */}
+          <div className={classes.inputBox}>
+            <label>Driver Id 1</label>
+            <input
+              type="number"
+              name="driverName1"
+              ref={driversInputRef1}
+              required
+            />
+          </div>
+          <div className={classes.inputBox}>
+            <label>Driver Id 2</label>
+            <input type="number" name="driverName2" ref={driversInputRef2} />
+          </div>
+          <div className={classes.inputBox}>
+            <label>Driver Id 3</label>
+            <input type="number" name="driverName3" ref={driversInputRef3} />
+          </div>
           <div className={classes.inputBox}>
             <label>Start Time</label>
-            <input type="text" name="startTime" ref={startTimeInputRef} />
+            <input
+              type="datetime-local"
+              name="startTime"
+              ref={startTimeInputRef}
+              required
+            />
           </div>
           <div className={classes.inputBox}>
             <label>End Time</label>
-            <input type="text" name="endTime" ref={endTimeInputRef} />
+            <input
+              type="datetime-local"
+              name="endTime"
+              ref={endTimeInputRef}
+              required
+            />
           </div>
 
           <div className={classes.buttonContainer}>
--- src/components/Management/TripManage.js
+++ src/components/Management/TripManage.js
@@ -1,55 +1,77 @@
 import React, { useEffect, useState } from "react";
 import classes from "./TripManage.module.css";
+import { ClipLoader } from "react-spinners";
 import SearchIcon from "@mui/icons-material/Search";
 import EditRoundedIcon from "@mui/icons-material/EditRounded";
 import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
 import { Link, useNavigate } from "react-router-dom";
 import AddTrip from "./AddTrip";
-const TripData = [
-  {
-    no: 1,
-    tripId: 11111,
-    vehicleId: 22222,
-    driverId: [33333, 55555, 66666],
-    startTime: "10:20",
-    endTime: "04:30",
-    status: "Ongoing",
-  },
-  {
-    no: 1,
-    tripId: 11111,
-    vehicleId: 22222,
-    driverId: [33333],
-    startTime: "10:20",
-    endTime: "04:30",
-    status: "Completed",
-  },
-  {
-    no: 1,
-    tripId: 11111,
-    vehicleId: 22222,
-    driverId: [33333, 55555],
-    startTime: "10:20",
-    endTime: "04:30",
-    status: "Yet to start",
-  },
-];
+import { axiosInstance } from "../../config";
+// const TripData = [
+//   {
+//     no: 1,
+//     tripId: 11111,
+//     vehicleRegNo: 22222,
+//     driverName: [33333, 55555, 66666],
+//     startTime: "10:20",
+//     endTime: "04:30",
+//     status: "Ongoing",
+//   },
+//   {
+//     no: 2,
+//     tripId: 11111,
+//     vehicleRegNo: 22222,
+//     driverName: [33333],
+//     startTime: "10:20",
+//     endTime: "04:30",
+//     status: "Completed",
+//   },
+//   {
+//     no: 3,
+//     tripId: 11111,
+//     vehicleRegNo: 22222,
+//     driverName: [33333, 55555],
+//     startTime: "10:20",
+//     endTime: "04:30",
+//     status: "Scheduled",
+//   },
+// ];
 const TripManage = () => {
-  const [filteredData, setFilteredData] = useState(TripData);
+  const [tripData, setTripData] = useState([]);
+  // const [filteredData, setFilteredData] = useState(TripData);
   const [searchValue, setSearchValue] = useState("");
   const [showAddForm, setShowAddForm] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const navigate = useNavigate();
+  let slNo = 0;
+
+  //const navigate = useNavigate();
+
+  /** Get all trip details */
+  useEffect(() => {
+    const getTrip = async () => {
+      const response = await axiosInstance.get("/trip");
+      console.log(response.data);
+      setTripData(response.data);
+      setIsLoading(false);
+    };
+    getTrip();
+  }, []);
+
+  /** Show or add form */
   const handleAddTrip = () => {
     setShowAddForm(true);
-    // navigate("/manage/customer");
   };
   const hideAddHandler = () => {
     setShowAddForm(false);
   };
+
+  /** Delete a trip */
   // const deleteTripHandler = (id) => {
   //   const trips = TripData.filter((t) => t.tripId === id);
   // };
+
+  /** Search a trip */
   const searchHandler = (e) => {
     setSearchValue(e.target.value);
   };
@@ -60,7 +82,8 @@ const TripManage = () => {
   //   );
   //   setFilteredData(filteredTrips);
   // }, [searchValue]);
-
+  // const newDate = new Date(tripData.PlannedStartTime.split(" ")[0]);
+  // console.log(newDate);
   return (
     <>
       <div className={classes.tManage}>
@@ -79,99 +102,97 @@ const TripManage = () => {
                 name="search"
                 onChange={searchHandler}
               />
-              {/* <button type="submit">
-              </button> */}
               <SearchIcon style={{ color: "#114A62", marginRight: "5px" }} />
             </form>
           </div>
         </div>
         <div className={classes.tMTable}>
-          <table>
-            <thead>
-              <tr>
-                <th>Sl No.</th>
-                <th>Trip Id</th>
-                <th>Vehicle Id</th>
-                <th>Driver Id</th>
-                <th>Start Time</th>
-                <th>End Time</th>
-                <th>Status</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredData.map((t) => (
-                <tr key={t.no}>
-                  <td>{t.no}</td>
-                  <td>{t.tripId}</td>
-                  <td>{t.vehicleId}</td>
-                  {/* <td>{t.driverId}</td> */}
-                  <td>
-                    {t.driverId.map((d) => (
-                      <>
-                        <span>{d}</span>
-                        <br />
-                      </>
-                    ))}
-                  </td>
-                  <td>{t.startTime}</td>
-                  <td>{t.endTime}</td>
-                  <td>
-                    {/* <button
-                      className={`${classes.statusButton} ${
-                        c.status === "Suspended"
-                          ? classes.suspend
-                          : classes.active
-                      }`}
-                    >
-                      {c.status}
-                    </button> */}
+          {isLoading ? (
+            <ClipLoader
+              color="#114a62"
+              loading
+              cssOverride={{ textAlign: "center" }}
+              size={150}
+              speedMultiplier={0.5}
+              // aria-label="Loading Spinner"
+              // data-testid="loader"
+            />
+          ) : (
+            <table>
+              <thead>
+                <tr>
+                  <th>Sl No.</th>
+                  <th>Trip Id</th>
+                  <th>Vehicle Reg No.</th>
+                  <th>Driver Id</th>
+                  <th>Start Time</th>
+                  <th>End Time</th>
+                  <th>Status</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tripData.map((t) => (
+                  <tr key={slNo}>
+                    <td>{slNo}</td>
+                    <td>{t.TripID}</td>
+                    <td>{t.Veh_Reg_No}</td>
                     <td>
-                      {t.status === "Ongoing" && (
+                      {t.DriverID.map((d) => (
+                        <>
+                          <span>{d}</span>
+                          <br />
+                        </>
+                      ))}
+                    </td>
+                    <td>{t.PlannedStartTime}</td>
+                    <td>{t.PlannedEndTime}</td>
+                    <td>
+                      {t.TripStatus.toLowerCase() === "ongoing" && (
                         <button
                           className={`${classes.statusButton} ${classes.ongoing}`}
                         >
-                          {t.status}
+                          Ongoing
                         </button>
                       )}
-                      {t.status === "Completed" && (
+                      {t.TripStatus.toLowerCase() === "completed" && (
                         <button
                           className={`${classes.statusButton} ${classes.completed}`}
                         >
-                          {t.status}
+                          Completed
                         </button>
                       )}
-                      {t.status === "Yet to start" && (
+                      {t.TripStatus.toLowerCase() === "scheduled" && (
                         <button
-                          className={`${classes.statusButton} ${classes.toStart}`}
+                          className={`${classes.statusButton} ${classes.scheduled}`}
                         >
-                          {t.status}
+                          Scheduled
                         </button>
                       )}
                     </td>
-                  </td>
-                  <td>
-                    <Link to={`/manage/trip/${t.tripId}}`}>
-                      <button
-                        style={{ color: "#114a62" }}
-                        className={classes.iconButton}
-                      >
-                        <EditRoundedIcon />
-                      </button>
-                    </Link>
+                    <td>
+                      <Link to={`/manage/trip/${t.TripId}}`}>
+                        <button
+                          style={{ color: "#114a62" }}
+                          className={classes.iconButton}
+                        >
+                          <EditRoundedIcon />
+                        </button>
+                      </Link>
 
-                    <button
-                      style={{ color: "#DC0000" }}
-                      className={classes.iconButton}
-                      // onClick={() => deleteTripHandler(t.tripId)}
-                    >
-                      <DeleteRoundedIcon />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                      <button
+                        style={{ color: "#DC0000" }}
+                        className={classes.iconButton}
+                        // onClick={() => deleteTripHandler(t.tripId)}
+                      >
+                        <DeleteRoundedIcon />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
       {showAddForm && <AddTrip onClose={hideAddHandler} />}
